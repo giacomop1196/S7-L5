0ab076b6688a2dfb3a6a7ab3ee1d15f5638bc5ba
giacomop1196 <giacomo.pillitteri65@gmail.com>
2025-07-11 16:02:13 +0200

    fix

--- js/dashboard.js
+++ js/dashboard.js
@@ -145,6 +145,10 @@ const getProducts = function () {
                                                 </div>
                                                 <div class="modal-body">
                                                     <form id="edit-product-form" data-product-id="${product._id}">
+                                                    <div class="d-flex flex-column align-items-center justify-content-center">
+                                                     <svg xmlns="http://www.w3.org/2000/svg" class="my-2 text-center" width="50" height="50" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                                                            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+                                                     </svg></div>
                                                         <!--NOME PRODOTTO-->
                                                         <div class="mb-3">
                                                             <label for="name-${product._id}" class="form-label">Nome Prodotto</label>
@@ -153,8 +157,8 @@ const getProducts = function () {
                                                         <!--DESCRIZIONE PRODOTTO-->
                                                         <div class="mb-3">
                                                             <label for="description-${product._id}" class="form-label">Descrizione Prodotto</label>
-                                                            <input required type="text" class="form-control" id="description-${product._id}"
-                                                                aria-describedby="emailHelp" value="${product.description}">
+                                                            <textarea required type="text" class="form-control" style="min-height: 160px;" id="description-${product._id}"
+                                                                aria-describedby="emailHelp" value="${product.description}">${product.description}</textarea>
                                                         </div>
                                                         <!--MARCA PRODOTTO-->
                                                         <div class="mb-3">
@@ -186,7 +190,7 @@ const getProducts = function () {
                                     </div>
                     `
                 })
-                
+
             }
         })
         .catch((err) => {
@@ -246,8 +250,8 @@ const editProduct = function (productId) {
         .then((res) => {
             if (res.ok) {
                 alert('Prodotto modificato con successo!');
-                productList.innerHTML = '';
-                getProducts();
+                // Aggiorno la pagina
+                location.assign('/dashboard.html')
             } else {
                 throw new Error('Errore durante la modifica del prodotto', res.status);
             }
